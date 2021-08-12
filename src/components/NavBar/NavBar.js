be87c7de299b5656cf23947a38a8a6faf3419015
samliu21 <sam4button@gmail.com>
2021-08-12 11:18:09 -0400

    Created dropdown

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,77 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 
+import * as authActions from "../../store/actions/auth";
 import styles from "./NavBar.module.css";
+import ui from "../../ui.module.css";
 
 export default function NavBar() {
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	const email = useSelector((state) => state.auth.email);
-	console.log(email);
 
 	const history = useHistory();
+	const dispatch = useDispatch();
+
+	const menuOpenHandler = () => {
+		setMenuOpen(true);
+	};
+
+	const menuCloseHandler = () => {
+		setMenuOpen(false);
+	};
 
 	const loginClickHandler = () => {
 		history.push("/login");
+	};
+
+	const logoutHandler = () => {
+		setMenuOpen(false);
+		dispatch(authActions.logout());
 	};
 
 	const homeClickHandler = () => {
 		history.push("/");
 	};
 
+	const AuthComponent = () => {
+		if (email) {
+			return (
+				<div
+					className={styles.dropdown}
+					onMouseEnter={menuOpenHandler}
+					onMouseLeave={menuCloseHandler}
+				>
+					<div className={`${styles.item}`}>{`Welcome ${email}!`}</div>
+					{menuOpen && (
+						<div className={`${styles.logout} ${ui.pointer}`} onClick={logoutHandler}>
+							Logout
+						</div>
+					)}
+				</div>
+			);
+		} else {
+			return (
+				<div
+					className={`${styles.item} ${ui.pointer}`}
+					onClick={loginClickHandler}
+				>
+					Login
+				</div>
+			);
+		}
+	};
+
 	return (
 		<div className={styles.container}>
-			<div className={styles.item} onClick={homeClickHandler}>
+			<div
+				className={`${styles.item} ${ui.pointer}`}
+				onClick={homeClickHandler}
+			>
 				Home
 			</div>
-			<div className={styles.item} onClick={loginClickHandler}>
-				{email ? `Welcome ${email}!` : "Login"}
-			</div>
+			<AuthComponent />
 		</div>
 	);
 }
