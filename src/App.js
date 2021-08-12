92533cfe177f78153fc34370d918ea5faa23cecf
samliu21 <sam4button@gmail.com>
2021-08-11 21:12:17 -0400

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { useEffect } from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ButtonList from "./components/ButtonList/ButtonList";
 import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
+	useEffect(() => {}, []);
+
 	return (
 		<HashRouter>
-			<div>
-				<NavBar />
-			</div>
-			<div id="main">
-				<Switch>
-					<Route path="/" component={ButtonList} />
-				</Switch>
-			</div>
+			{/* <Background> */}
+				<div>
+					<NavBar />
+				</div>
+				<div id="main">
+					<Switch>
+						<Route path="/" component={ButtonList} />
+					</Switch>
+				</div>
+			{/* </Background> */}
 		</HashRouter>
 	);
 }
